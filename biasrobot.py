de9c4a64764570a830f833176601757126684b4d
Iain Marshall <mail@ijmarshall.com>
2015-07-12 22:37:27 +0100

    do all the domains (though not most efficent way as yet, but it works!)

--- biasrobot.py
+++ biasrobot.py
@@ -18,53 +18,64 @@ class BiasRobot:
 
         self.bias_domains = ['Random sequence generation', 'Allocation concealment', 'Blinding of participants and personnel', 'Blinding of outcome assessment', 'Incomplete outcome data', 'Selective reporting']
 
-    def annotate(self, doc_text, domain=None):
+    def annotate(self, doc_text, domains=None):
 
-        if domain is None:
-            domain = self.bias_domains
+        # Some processing to allow flexibility of domains parameter
+
+        # check all domains by default
+        if domains is None: 
+            domains = self.bias_domains
+        
+        # make into a list if a single item passed
+        if isinstance(domains, list)==False:
+            domains = [domains]
 
         # allow domain to be either a domain itself or index of one
-        if isinstance(domain, int):
-            domain = self.bias_domains[domain]
-        else:
-            assert domain in self.bias_domains
+        # (first element checked)
+        if isinstance(domains[0], int):
+            domains = [self.bias_domains[domain] for domain in domains]
+
+        marginalia = []
+        
+        for domain in domains:
+
+            doc_sents = sent_tokenize(doc_text)
+            doc_domains = [domain] * len(doc_sents)
+            doc_X_i = zip(doc_sents, doc_domains)
 
 
 
-        doc_sents = sent_tokenize(doc_text)
-        doc_domains = [domain] * len(doc_sents)
-        doc_X_i = zip(doc_sents, doc_domains)
+            self.vec.builder_clear()
 
+            self.vec.builder_add_docs(doc_sents)
+            self.vec.builder_add_docs(doc_X_i)
+            doc_sents_X = self.vec.builder_transform()
+            doc_sents_preds = self.sent_clf.predict(doc_sents_X).A1
 
+            high_prob_sents = [sent for sent, sent_pred in 
+                                        zip(doc_sents, doc_sents_preds) if sent_pred==1]
 
-        self.vec.builder_clear()
+            high_prob_sents_j = " ".join(high_prob_sents)
 
-        self.vec.builder_add_docs(doc_sents)
-        self.vec.builder_add_docs(doc_X_i)
-        doc_sents_X = self.vec.builder_transform()
-        doc_sents_preds = self.sent_clf.predict(doc_sents_X).A1
+            sent_domain_interaction = "-s-" + domain
 
-        high_prob_sents = [sent for sent, sent_pred in 
-                                    zip(doc_sents, doc_sents_preds) if sent_pred==1]
+            # build up test vector
+            self.vec.builder_clear()
+            self.vec.builder_add_docs([doc_text]) # add base features
+            self.vec.builder_add_docs([(doc_text, domain)]) # add interactions
+            self.vec.builder_add_docs([(high_prob_sents_j, sent_domain_interaction)]) # sentence interactions
+        
+            X = self.vec.builder_transform()
 
-        high_prob_sents_j = " ".join(high_prob_sents)
+            bias_class = ["HIGH/UNCLEAR", "LOW"]
+            bias_pred = bias_class[(self.doc_clf.predict(X).A1[0] + 1) / 2]
 
-        sent_domain_interaction = "-s-" + domain
+            marginalia.append({"type": "Risk of Bias",
+                               "title": domain,
+                "annotations": [{"content": sent} for sent in high_prob_sents],
+                "description": "**Overall risk of bias prediction**: " + bias_pred})
 
-        # build up test vector
-        self.vec.builder_clear()
-        self.vec.builder_add_docs([doc_text]) # add base features
-        self.vec.builder_add_docs([(doc_text, domain)]) # add interactions
-        self.vec.builder_add_docs([(high_prob_sents_j, sent_domain_interaction)]) # sentence interactions
-    
-        X = self.vec.builder_transform()
-
-        bias_class = ["HIGH/UNCLEAR", "LOW"]
-        bias_pred = bias_class[(self.doc_clf.predict(X).A1[0] + 1) / 2]
-
-        return {"domain": domain,
-                "bias level": bias_pred,
-                "justification": high_prob_sents}
+        return {"marginalia": marginalia}
 
 def main():
 
@@ -75,7 +86,7 @@ def main():
 
 
     robot = BiasRobot()
-    print robot.annotate(text, 0)
+    print robot.annotate(text)
 
 
 if __name__ == '__main__':
