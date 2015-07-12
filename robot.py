13ea63373f49a3ae07df59eb25323178bf052bd5
Iain Marshall <mail@ijmarshall.com>
2015-07-12 22:49:44 +0100

    check all domains

--- robot.py
+++ robot.py
@@ -14,7 +14,7 @@ print "DONE!"
 @app.route('/annotate', methods = ['POST'])
 def annotate():
 	json_data = request.json
-	annotations = BOT.annotate(json_data["text"], 0) # just does randomization for now
+	annotations = BOT.annotate(json_data["text"]) # just does randomization for now
 	return json.dumps(annotations)
 
 if __name__ == '__main__':
