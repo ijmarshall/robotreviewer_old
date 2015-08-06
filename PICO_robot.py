8b6ed3cac21d8eb7b7d51f4ca34955a760121aeb
Joel Kuiper <me@joelkuiper.eu>
2015-08-07 01:17:32 +0200

    Use absolute path

--- PICO_robot.py
+++ PICO_robot.py
@@ -1,6 +1,6 @@
 """
 the PICORobot class takes the full text of a clinical trial as
-input as a string, and returns Population, Comparator/Intervention 
+input as a string, and returns Population, Comparator/Intervention
 Outcome information as a dict which can be easily converted to JSON.
 
     text = "Streptomycin Treatment of Pulmonary Tuberculosis: A Medical Research Council Investigation..."
@@ -8,8 +8,8 @@ Outcome information as a dict which can be easily converted to JSON.
     robot = PICORobot()
     annotations = robot.annotate(text)
 
-The model was derived using the "Supervised Distant Supervision" strategy 
-introduced in our paper "Extracting PICO Sentences from Clinical Trial Reports 
+The model was derived using the "Supervised Distant Supervision" strategy
+introduced in our paper "Extracting PICO Sentences from Clinical Trial Reports
 using Supervised Distant Supervision".
 """
 
@@ -30,13 +30,14 @@ import numpy as np
 import pico_vectorizer
 import drugbank
 import sys
+import os
 
 ###
 # a little bit dirty, sorry.
 sys.modules['cochranenlp.ml.pico_vectorizer'] = pico_vectorizer
 sys.modules['cochranenlp.textprocessing.drugbank'] = drugbank
 
-from pico_vectorizer import PICO_vectorizer 
+from pico_vectorizer import PICO_vectorizer
 
 class PICORobot:
 
@@ -44,12 +45,12 @@ class PICORobot:
         '''
         Note: we group Intervention and Comparator together
         '''
-        
-        ### @TODO! 
+
+        ### @TODO!
         '''
         self.P_clf = MiniClassifier("robots/PICO/P_model.rbt", coef_dim=50019)
         P_vec_f = bz2.BZ2File("robots/PICO/P_vectorizer.pbz2", 'r')
-        ### 
+        ###
         # this is a bit slow. sorry.
         print "unpickling P vectorizer..."
         self.P_vec = pickle.load(P_vec_f)
@@ -68,9 +69,10 @@ class PICORobot:
 
     @staticmethod
     def _load_model_and_v(m_path, v_path, coef_dim=50019):
-        m = MiniClassifier(m_path, coef_dim=50019)
+        abs_dir = os.path.dirname(__file__) # absolute path to here
+        m = MiniClassifier(os.path.join(abs_dir, m_path), coef_dim=50019)
         print "loading vectorizer: %s ..." % v_path
-        v_file = bz2.BZ2File(v_path, 'r')
+        v_file = bz2.BZ2File(os.path.join(abs_dir, v_path), 'r')
         print "ok."
         v = pickle.load(v_file)
         return m, v
@@ -82,14 +84,14 @@ class PICORobot:
         Annotate full text of clinical trial report
         `top_k` refers to 'top-k recall'.
 
-        Default alpha was totally scientifically set. 
-        """   
+        Default alpha was totally scientifically set.
+        """
         marginalia = []
-        
+
         doc_sents = sent_tokenize(doc_text)
         # quintile indicators (w.r.t. document) for sentences
         positional_features = PICORobot._get_positional_features(doc_sents)
-       
+
         ###
         # @TODO just population (P) for now
         for domain, model, vectorizer in zip(
@@ -106,16 +108,16 @@ class PICORobot:
             #pdb.set_trace()
             if len(filtered_high_prob_sent_indices) < min_k:
                 high_prob_sent_indices = high_prob_sent_indices[:min_k]
-            else: 
+            else:
                 high_prob_sent_indices = filtered_high_prob_sent_indices
 
             high_prob_sents = [doc_sents[i] for i in high_prob_sent_indices]
-            
+
 
             '''
             doc_sents_preds = model.decision_function(doc_sents_X)
             high_prob_sent_indices = np.argsort(doc_sents_preds)[:-top_k-1:-1] # top k, with no 1 first
-            
+
             '''
             marginalia.append({
                 "type": "PICO",
@@ -129,7 +131,7 @@ class PICORobot:
     def _get_positional_features(sentences):
         ''' generate positional features here (quintiles) for doc sentences. '''
         num_sents = len(sentences)
-        quintile_cutoff = num_sents / 5 
+        quintile_cutoff = num_sents / 5
 
         if quintile_cutoff == 0:
             sentence_quintiles = [{"DocTooSmallForQuintiles" : 1} for ii in xrange(num_sents)]
